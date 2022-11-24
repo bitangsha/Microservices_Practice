a9a042cf10ae3e70441542b8fb494ceb02279e9c
Bitangsha <bitangsha2018@outlook.com>
2022-11-24 08:50:14 +0530

    Added user model

---
+++ src/main/java/com/microservice/model/User.java
@@ -0,0 +1,53 @@
+package com.microservice.model;
+
+import java.time.LocalDate;
+
+public class User {
+	
+	private Integer id;
+	
+	private String name;
+	
+	private LocalDate dob;
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", dob=" + dob + "]";
+	}
+
+	public User(int id, String name, LocalDate dob) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dob = dob;
+	}
+	
+	
+
+}
